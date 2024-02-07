8dbb8302160b1671137b6390561fc9d8f8db4750
Lautaro Iezzi <iezzilautaro@gmail.com>
2024-02-07 13:59:19 -0300

    Cambio en el JS

--- index.js
+++ index.js
@@ -1,184 +1,61 @@
-const AYG = "A&G Travel";
+const AYG = "Impresiones 3D.LY";
 alert(AYG);
 
 let name = prompt("Ingrese su nombre");
 let surname = prompt("Ingrese su apellido");
 
-let economyBrasil = 350000;
+const PRODUCTOS = [
+    { id: "1", nombre: "Soporte celular", tiempo: 0.6, gramos: 5 },
+    { id: "2", nombre: "Posa vaso", tiempo: 1.2, gramos: 10 },
+    { id: "3", nombre: "Cortantes", tiempo: 1.8, gramos: 15 }
+];
 
-do {
-    const DESTINOS = prompt("¿A donde desea viajar?; 1. Rio de Janeino, 2. Cancun o 3. New York");
+function calcularPrecioProducto(productoId) {
+    const producto = PRODUCTOS.find(producto => producto.id === productoId);
 
-    let resultado;
-
-    switch (DESTINOS) {
-        case "1":
-            const BRASIL = prompt("¿Con que aerolinea desea viajar?; Aerolineas Argentinas o Gol").toLowerCase();
-            if (BRASIL == "aerolineas argentinas") {
-                const PRECIO = prompt("¿Desea viajar en Economy o en Primera Clase?").toLowerCase();
-                if (PRECIO == "economy") {
-                    const ECONOMY = prompt("El valor del vuelo es de $" + economyBrasil + ", ¿Desea comprarlo?(Responda SI o NO)").toLowerCase();
-                    if (ECONOMY === "si") {
-                        calcular();
-                    } else if (ECONOMY === "no") {
-                        alert("¡Hasta pronto!");
-                    }
-                } else if (PRECIO == "primera clase") {
-                    const PRIMERA_CLASE = prompt("El valor del vuelo es de $450.000, ¿Desea comprarlo?(Responda SI o NO").toLowerCase();
-                    if (PRIMERA_CLASE === "si") {
-                        const CANTIDAD = prompt("¿Cuantos vuelos desea comprar?");
-                        resultado = 450000 * CANTIDAD;
-                        alert("El total a abonar es " + resultado);
-                    } else if (ECONOMY === "no") {
-                        alert("¡Hasta pronto!");
-                    }
-                }
-            } else if (BRASIL == "gol") {
-                const PRECIO = prompt("¿Desea viajar en Economy o en Primera Clase?").toLowerCase();
-                if (PRECIO == "economy") {
-                    const ECONOMY = prompt("El valor del vuelo es de $380.000, ¿Desea comprarlo?(Responda SI o NO").toLowerCase();
-                    if (ECONOMY === "si") {
-                        const CANTIDAD = prompt("¿Cuantos vuelos desea comprar?");
-                        resultado = 380000 * CANTIDAD;
-                        alert("El total a abonar es " + resultado);
-                    } else if (ECONOMY === "no") {
-                        alert("¡Hasta pronto!");
-                    }
-                } else if (PRECIO == "primera clase") {
-                    const PRIMERA_CLASE = prompt("El valor del vuelo es de $580.000, ¿Desea comprarlo?(Responda SI o NO").toLowerCase();
-                    if (PRIMERA_CLASE === "si") {
-                        const CANTIDAD = prompt("¿Cuantos vuelos desea comprar?");
-                        resultado = 580000 * CANTIDAD;
-                        alert("El total a abonar es " + resultado);
-                    } else if (ECONOMY === "no") {
-                        alert("¡Hasta pronto!"); // Preguntar como terminar el programa aca
-                    }
-                }
-            }
-            break;
-
-        case "2":
-            const MEXICO = prompt("¿Con que aerolinea desea viajar?; Gol o Avianca").toLowerCase();
-            if (MEXICO == "gol") {
-                const PRECIO = prompt("¿Desea viajar en Economy o en Primera Clase?").toLowerCase();
-                if (PRECIO == "economy") {
-                    const ECONOMY = prompt("El valor del vuelo es de $1.100.000, ¿Desea comprarlo?(Responda SI o NO").toLowerCase();
-                    if (ECONOMY === "si") {
-                        const CANTIDAD = prompt("¿Cuantos vuelos desea comprar?");
-                        resultado = 1100000 * CANTIDAD;
-                        alert("El total a abonar es " + resultado);
-                    } else if (ECONOMY === "no") {
-                        alert("¡Hasta pronto!");
-                    }
-                } else if (PRECIO == "primera clase") {
-                    const PRIMERA_CLASE = prompt("El valor del vuelo es de $1.400.000, ¿Desea comprarlo?(Responda SI o NO").toLowerCase();
-                    if (PRIMERA_CLASE === "si") {
-                        const CANTIDAD = prompt("¿Cuantos vuelos desea comprar?");
-                        resultado = 1400000 * CANTIDAD;
-                        alert("El total a abonar es " + resultado);
-                    } else if (ECONOMY === "no") {
-                        alert("¡Hasta pronto!");
-                    }
-                }
-            } else if (MEXICO == "avianca") {
-                const PRECIO = prompt("¿Desea viajar en Economy o en Primera Clase?").toLowerCase();
-                if (PRECIO == "economy") {
-                    const ECONOMY = prompt("El valor del vuelo es de $1.000.000, ¿Desea comprarlo?(Responda SI o NO").toLowerCase();
-                    if (ECONOMY === "si") {
-                        const CANTIDAD = prompt("¿Cuantos vuelos desea comprar?");
-                        resultado = 1000000 * CANTIDAD;
-                        alert("El total a abonar es " + resultado);
-                    } else if (ECONOMY === "no") {
-                        alert("¡Hasta pronto!");
-                    }
-                } else if (PRECIO == "primera clase") {
-                    const PRIMERA_CLASE = prompt("El valor del vuelo es de $1.300.000, ¿Desea comprarlo?(Responda SI o NO").toLowerCase();
-                    if (PRIMERA_CLASE === "si") {
-                        const CANTIDAD = prompt("¿Cuantos vuelos desea comprar?");
-                        resultado = 1300000 * CANTIDAD;
-                        alert("El total a abonar es " + resultado);
-                    } else if (ECONOMY === "no") {
-                        alert("¡Hasta pronto!");
-                    }
-                }
-            }
-            break;
-
-        case "3":
-            const EEUU = prompt("¿Con que aerolinea desea viajar?; Aerolineas Argentinas, Gol o Avianca").toLowerCase();
-            if (EEUU == "aerolineas argentinas") {
-                const PRECIO = prompt("¿Desea viajar en Economy o en Primera Clase?").toLowerCase();
-                if (PRECIO == "economy") {
-                    const ECONOMY = prompt("El valor del vuelo es de $2.100.000, ¿Desea comprarlo?(Responda SI o NO").toLowerCase();
-                    if (ECONOMY === "si") {
-                        const CANTIDAD = prompt("¿Cuantos vuelos desea comprar?");
-                        resultado = 2100000 * CANTIDAD;
-                        alert("El total a abonar es " + resultado);
-                    } else if (ECONOMY === "no") {
-                        alert("¡Hasta pronto!");
-                    }
-                } else if (PRECIO == "primera clase") {
-                    const PRIMERA_CLASE = prompt("El valor del vuelo es de $2.600.000, ¿Desea comprarlo?(Responda SI o NO").toLowerCase();
-                    if (PRIMERA_CLASE === "si") {
-                        const CANTIDAD = prompt("¿Cuantos vuelos desea comprar?");
-                        resultado = 2600000 * CANTIDAD;
-                        alert("El total a abonar es " + resultado);
-                    } else if (ECONOMY === "no") {
-                        alert("¡Hasta pronto!");
-                    }
-                }
-            } else if (EEUU == "gol") {
-                const PRECIO = prompt("¿Desea viajar en Economy o en Primera Clase?").toLowerCase();
-                if (PRECIO == "economy") {
-                    const ECONOMY = prompt("El valor del vuelo es de $2.000.000, ¿Desea comprarlo?(Responda SI o NO").toLowerCase();
-                    if (ECONOMY === "si") {
-                        const CANTIDAD = prompt("¿Cuantos vuelos desea comprar?");
-                        resultado = 2000000 * CANTIDAD;
-                        alert("El total a abonar es " + resultado);
-                    } else if (ECONOMY === "no") {
-                        alert("¡Hasta pronto!");
-                    }
-                } else if (PRECIO == "primera clase") {
-                    const PRIMERA_CLASE = prompt("El valor del vuelo es de $2.500.000, ¿Desea comprarlo?(Responda SI o NO").toLowerCase();
-                    if (PRIMERA_CLASE === "si") {
-                        const CANTIDAD = prompt("¿Cuantos vuelos desea comprar?");
-                        resultado = 2500000 * CANTIDAD;
-                        alert("El total a abonar es " + resultado);
-                    } else if (ECONOMY === "no") {
-                        alert("¡Hasta pronto!");
-                    }
-                }
-            } else if (EEUU == "avianca") {
-                const PRECIO = prompt("¿Desea viajar en Economy o en Primera Clase?").toLowerCase();
-                if (PRECIO == "economy") {
-                    const ECONOMY = prompt("El valor del vuelo es de $1.300.000, ¿Desea comprarlo?(Responda SI o NO").toLowerCase();
-                    if (ECONOMY === "si") {
-                        const CANTIDAD = prompt("¿Cuantos vuelos desea comprar?");
-                        resultado = 1300000 * CANTIDAD;
-                        alert("El total a abonar es " + resultado);
-                    } else if (ECONOMY === "no") {
-                        alert("¡Hasta pronto!");
-                    }
-                } else if (PRECIO == "primera clase") {
-                    const PRIMERA_CLASE = prompt("El valor del vuelo es de $1.800.000, ¿Desea comprarlo?(Responda SI o NO").toLowerCase();
-                    if (PRIMERA_CLASE === "si") {
-                        const CANTIDAD = prompt("¿Cuantos vuelos desea comprar?");
-                        resultado = 1800000 * CANTIDAD;
-                        alert("El total a abonar es " + resultado);
-                    } else if (ECONOMY === "no") {
-                        alert("¡Hasta pronto!");
-                    }
-                }
-            }
-            break;
-    }
-    function calcular (){
-        const CANTIDAD = prompt("¿Cuantos vuelos desea comprar?");
-        resultado = economyBrasil * CANTIDAD;
-        alert("El total a abonar es " + resultado);
+    if (!producto) {
+        return "Producto no encontrado";
     }
 
-    continuar = prompt("¿Desea buscar otro vuelo?, SI o NO").toLowerCase();
-} while (continuar === "si");
+    // Calcular el precio del material
+    const precioMaterial = producto.gramos * 20000 / 1000;
 
-alert("Espero que le haya servido");
+    // Calcular el precio de la luz
+    const precioLuz = ((23 * 150) / 1000) * producto.tiempo;
+
+    // Calcular el desgaste de la máquina
+    const desgasteMaquina = (4320 / 20000) * producto.tiempo;
+
+    // Calcular el margen de error
+    const margenError = (precioMaterial + precioLuz + desgasteMaquina) * 0.3;
+
+    // Calcular el precio final del producto
+    const precioFinal = (precioMaterial + precioLuz + desgasteMaquina + margenError) * 3;
+
+    return precioFinal;
+}
+
+// Pedir al usuario que ingrese el ID del producto
+const productoId = prompt("Productos disponibles: 1. Soporte celular, 2. Posa vaso, 3. Cortantes. Ingrese el ID del producto: ");
+
+// Usar un switch para seleccionar el producto
+switch (productoId) {
+    case "1":
+        const precioProducto1 = calcularPrecioProducto(productoId);
+        alert(`El precio del producto Soporte celular es $${precioProducto1}`);
+        console.log(`El precio del producto Soporte celular es $${precioProducto1}`);
+        break;
+    case "2":
+        const precioProducto2 = calcularPrecioProducto(productoId);
+        alert(`El precio del producto Posa vaso es $${precioProducto2}`);
+        console.log(`El precio del producto Posa vaso es $${precioProducto2}`);
+        break;
+    case "3":
+        const precioProducto3 = calcularPrecioProducto(productoId);
+        alert(`El precio del producto Cortantes es $${precioProducto3}`);
+        console.log(`El precio del producto Cortantes es $${precioProducto3}`);
+        break;
+    default:
+        console.log("Producto no encontrado");
+        break;
+}
