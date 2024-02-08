c3f3effef3661984b84e1ca7cec69f2b0cf09a05
Lautaro Iezzi <iezzilautaro@gmail.com>
2024-02-08 14:50:53 -0300

    Cambio la forma de agregar datos

--- index.js
+++ index.js
@@ -4,33 +4,48 @@ alert(AYG);
 let name = prompt("Ingrese su nombre");
 let surname = prompt("Ingrese su apellido");
 
+class Producto {
+    constructor(id, nombre, description, imagen, tiempo, gramos) {
+        this.id = id;
+        this.nombre = nombre;
+        this.description = description;
+        this.imagen = imagen;
+        this.tiempo = tiempo;
+        this.gramos = gramos;
+    }
+
+}
+
 const PRODUCTOS = [
-    { id: "1", nombre: "Soporte celular", tiempo: 0.6, gramos: 5 },
-    { id: "2", nombre: "Posa vaso", tiempo: 1.2, gramos: 10 },
-    { id: "3", nombre: "Cortantes", tiempo: 1.8, gramos: 15 }
+    new Producto("1", "Soporte celular", "Dos tamaños disponibles, Movil o Tablet", "url", 0.6, 5),
+    new Producto("2", "Posa vaso", "Diseño de Harry Potter", "url", 1.2, 10),
+    new Producto("3", "Cortante", "Diseño de números", "url", 1.8, 15)
 ];
+
+const DATOS = { precioKG: 25000, precioKWh: 25, consumo: 150, desgaste: 4500, margen: 0.3, ganancia: 3 }
 
 function calcularPrecioProducto(productoId) {
     const producto = PRODUCTOS.find(producto => producto.id === productoId);
+    const datos = DATOS;
 
     if (!producto) {
         return "Producto no encontrado";
     }
 
     // Calcular el precio del material
-    const precioMaterial = producto.gramos * 20000 / 1000;
+    const precioMaterial = producto.gramos * datos.precioKG / 1000;
 
     // Calcular el precio de la luz
-    const precioLuz = ((23 * 150) / 1000) * producto.tiempo;
+    const precioLuz = ((datos.precioKWh * datos.consumo) / 1000) * producto.tiempo;
 
     // Calcular el desgaste de la máquina
-    const desgasteMaquina = (4320 / 20000) * producto.tiempo;
+    const desgasteMaquina = (datos.desgaste / datos.precioKG) * producto.tiempo;
 
     // Calcular el margen de error
-    const margenError = (precioMaterial + precioLuz + desgasteMaquina) * 0.3;
+    const margenError = (precioMaterial + precioLuz + desgasteMaquina) * datos.margen;
 
     // Calcular el precio final del producto
-    const precioFinal = (precioMaterial + precioLuz + desgasteMaquina + margenError) * 3;
+    const precioFinal = (precioMaterial + precioLuz + desgasteMaquina + margenError) * datos.ganancia;
 
     return precioFinal;
 }
