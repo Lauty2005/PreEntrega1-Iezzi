16db5b819b667a53e6d822c19a665598c0fb9fa5
Lauti Notebook <iezzilautaro@gmail.com>
2024-02-28 18:06:22 -0300

    Actualizando el JS

---
+++ js/carrito.js
@@ -0,0 +1,117 @@
+/*const elementosCarrito = JSON.parse(localStorage.getItem('carrito')) || [];
+const contenedorElementosCarrito = document.getElementById('elementos-carrito');
+const totalSpan = document.getElementById('total');
+
+//eliminar el producto
+
+function eliminarDelCarrito(idProducto) {
+    const indice = elementosCarrito.findIndex(item => item.id === idProducto);
+    if (indice !== -1) {
+        elementosCarrito.splice(indice, 1);
+        actualizarCarritoEnLocalStorage();
+        renderizarCarrito();
+    }
+
+    // Recalcular el total
+    let precioTotal = 0;
+    elementosCarrito.forEach(item => {
+        precioTotal += item.precio * item.cantidad;
+    });
+
+    // Actualizar el total en la interfaz
+    totalSpan.textContent = precioTotal;
+}
+
+function renderizarCarrito() {
+    const itemsCarrito = JSON.parse(localStorage.getItem('carrito')) || [];
+
+    contenedorElementosCarrito.innerHTML = '';
+    let precioTotal = 0;
+    elementosCarrito.forEach(item => {
+        const li = document.createElement('li');
+        li.innerHTML = `
+            <h3>${item.nombre}</h3> 
+            <p id="cantidad"> x ${item.cantidad}</p>  
+            <p> $${item.precio * item.cantidad}</p>`
+        const btnEliminar = document.createElement('button');
+        btnEliminar.textContent = 'eliminar';
+        btnEliminar.addEventListener('click', () => eliminarDelCarrito(item.id))
+        li.appendChild(btnEliminar);
+        contenedorElementosCarrito.appendChild(li);
+        precioTotal += item.precio * item.cantidad;
+        btnEliminar.addEventListener('click', () => eliminarDelCarrito(item.id));
+    })
+    totalSpan.textContent = precioTotal;
+}
+
+function realizarCompra() {
+    alert(`compra finalizada $${totalSpan.textContent}`);
+    elementosCarrito.length = 0;
+    renderizarCarrito();
+}
+
+document.getElementById('btn-comprar').addEventListener('click', realizarCompra);
+
+contenedorProductos.addEventListener('click', function (evento) {
+    if (evento.target.classList.contains('btn-agregar-carrito')) {
+        const idProducto = parseInt(evento.target.getAttribute('data-id'));
+        agregarAlCarrito(idProducto);
+    }
+});
+
+function actualizarCarritoEnLocalStorage() {
+    // Convertir el array de elementos a JSON
+    const carritoJSON = JSON.stringify(elementosCarrito);
+
+    // Guardar el JSON en el localStorage
+    localStorage.setItem('carrito', carritoJSON);
+}
+
+// Renderizar productos al cargar la página
+window.addEventListener('load', () => {
+    renderizarProductos();
+    renderizarCarrito();
+});*/
+
+const elementosCarrito = JSON.parse(localStorage.getItem('carrito')) || [];
+const contenedorElementosCarrito = document.getElementById('elementos-carrito');
+const totalSpan = document.getElementById('total');
+
+function renderizarCarrito() {
+    contenedorElementosCarrito.innerHTML = '';
+    let precioTotal = 0;
+    elementosCarrito.forEach(item => {
+        const producto = productos.find(p => p.id === item);
+        const li = document.createElement('li');
+        li.innerHTML = `
+          <h3>${producto.nombre}</h3> 
+          <p id="cantidad"> x ${item.cantidad}</p>  
+          <p> $${producto.precio * item.cantidad}</p>`
+        const btnEliminar = document.createElement('button');
+        btnEliminar.textContent = 'eliminar';
+        btnEliminar.addEventListener('click', () => eliminarDelCarrito(item.id))
+        li.appendChild(btnEliminar);
+        contenedorElementosCarrito.appendChild(li);
+        precioTotal += producto.precio * item.cantidad;
+    })
+    totalSpan.textContent = precioTotal;
+}
+
+function eliminarDelCarrito(idProducto) {
+    // Eliminar el producto del carrito (logica de almacenamiento)
+    const indice = elementosCarrito.findIndex(item => item.id === idProducto);
+    if (indice !== -1) {
+        elementosCarrito.splice(indice, 1);
+        localStorage.setItem('carrito', JSON.stringify(elementosCarrito));
+        renderizarCarrito();
+    }
+}
+
+function realizarCompra() {
+    // Completar la compra (logica de pago)
+    alert(`compra finalizada $${totalSpan.textContent}`);
+    // Mostrar mensaje de compra exitosa
+    // Vaciar el carrito (logica de almacenamiento)
+    elementosCarrito.length = 0;
+    localStorage.setItem('carrito', JSON.stringify(elementosCarrito));
+}
---
+++ js/index.js
@@ -0,0 +1,79 @@
+/*const IMP = "Impresiones 3D.LY";
+
+class Producto {
+    constructor(id, nombre, description, imagen, tiempo, gramos) {
+        this.id = id;
+        this.nombre = nombre;
+        this.description = description;
+        this.imagen = imagen;
+        this.tiempo = tiempo;
+        this.gramos = gramos;
+    }
+
+}
+
+const PRODUCTOS = [
+    new Producto("1", "Soporte celular", "Dos tamaños disponibles, Movil o Tablet", "url", 0.6, 5),
+    new Producto("2", "Posa vaso", "Diseño de Harry Potter", "url", 1.2, 10),
+    new Producto("3", "Cortante", "Diseño de números", "url", 1.8, 15)
+];
+
+const DATOS = { precioKG: 25000, precioKWh: 25, consumo: 150, desgaste: 4500, margen: 0.3, ganancia: 3 }
+
+function calcularPrecioProducto(productoId) {
+    const producto = PRODUCTOS.find(producto => producto.id === productoId);
+    const datos = DATOS;
+
+    if (!producto) {
+        return "Producto no encontrado";    
+    }
+
+    // Calcular el precio del material
+    const precioMaterial = producto.gramos * datos.precioKG / 1000;
+
+    // Calcular el precio de la luz
+    const precioLuz = ((datos.precioKWh * datos.consumo) / 1000) * producto.tiempo;
+
+    // Calcular el desgaste de la máquina
+    const desgasteMaquina = (datos.desgaste / datos.precioKG) * producto.tiempo;
+
+    // Calcular el margen de error
+    const margenError = (precioMaterial + precioLuz + desgasteMaquina) * datos.margen;
+
+    // Calcular el precio final del producto
+    const precioFinal = (precioMaterial + precioLuz + desgasteMaquina + margenError) * datos.ganancia;
+
+    return precioFinal;
+}*/
+
+const productos = [
+    { id: 1, nombre: "azucar", precio: 200 },
+    { id: 2, nombre: "arroz", precio: 100 },
+    { id: 3, nombre: "dulce de leche", precio: 140 },
+    { id: 4, nombre: "cereales", precio: 40 },
+];
+
+const contenedorProductos = document.getElementById('productos');
+
+function renderizarProductos() {
+    productos.forEach(producto => {
+        const div = document.createElement('div');
+        div.classList.add('producto');
+        div.innerHTML = `
+        <h3>${producto.nombre}</h3>
+        <p>${producto.precio}</p>
+        <button class="btn-agregar-carrito" data-id="${producto.id}">Agregar al carrito</button>
+      `;
+        div.addEventListener('click', () => agregarAlCarrito(producto.id));
+        contenedorProductos.appendChild(div);
+    })
+}
+
+function agregarAlCarrito(idProducto) {
+    // Agregar el producto al carrito (logica de almacenamiento)
+    const itemsCarrito = JSON.parse(localStorage.getItem('carrito')) || [];
+    itemsCarrito.push(idProducto);
+    localStorage.setItem('carrito', JSON.stringify(itemsCarrito));
+}
+
+renderizarProductos();
